4203fb9ada17855cb49e24cef84a787a4aa57e91
Eric Erhardt <eric.erhardt@microsoft.com>
2017-11-22 15:42:36 -0600

    Add initial code

---
+++ LinearAlgebra.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Numerics;
+using System.Runtime.InteropServices;
+
+namespace LAPack
+{
+    static class LinearAlgebra
+    {
+        const int LAPACK_ROW_MAJOR = 101;
+        const int LAPACK_COL_MAJOR = 102;
+
+        /// <summary>
+        /// Solves the system of linear equations AX = B for X, where A, B, and X are general matrices.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static DenseTensor<double> Solve(DenseTensor<double> a, DenseTensor<double> b)
+        {
+            if (a.Rank != 2) throw new ArgumentException("a must be a square matrix", nameof(a));
+            if (a.Dimensions[0] != a.Dimensions[1]) throw new ArgumentException("a must be a square matrix", nameof(a));
+            if (b.Rank != 2) throw new ArgumentException("b must be a matrix", nameof(b));
+            if (a.Dimensions[0] != b.Dimensions[0]) throw new ArgumentException("The number of rows in b must match the number of rows in a", nameof(b));
+
+            // need to clone the inputs because LAPack will mutate the values
+            var aClone = (DenseTensor<double>)a.Clone();
+            var bClone = (DenseTensor<double>)b.Clone();
+
+            unsafe
+            {
+                Span<int> pivotIntegers = stackalloc int[a.Dimensions[1]];
+                fixed (double* aPtr = &aClone.Buffer.Span.DangerousGetPinnableReference())
+                fixed (double* bPtr = &bClone.Buffer.Span.DangerousGetPinnableReference())
+                fixed (int* ipiv = &pivotIntegers.DangerousGetPinnableReference())
+                {
+                    LAPACKE_dgesv(
+                        a.IsReversedStride ? LAPACK_COL_MAJOR : LAPACK_ROW_MAJOR, 
+                        a.Dimensions[0], 
+                        b.Dimensions[1], 
+                        aPtr, 
+                        a.Dimensions[1], 
+                        ipiv, 
+                        bPtr, 
+                        b.Dimensions[1]);
+                }
+            }
+
+            return bClone;
+        }
+
+        [DllImport("liblapacke.dll")]
+        static extern unsafe int LAPACKE_dgesv(int matrix_layout, int n, int nrhs, double* a, int lda, int* ipvt, double* bx, int ldb);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Numerics;
+
+namespace LAPack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // sample taken from https://software.intel.com/sites/products/documentation/doclib/mkl_sa/11/mkl_lapack_examples/lapacke_dgesv_row.c.htm
+            DenseTensor<double> a = new[,]
+            {
+                { 6.80, -6.05, -0.45,  8.32, -9.67, },
+                {-2.11, -3.30,  2.58,  2.71, -5.14, },
+                { 5.66,  5.36, -2.70,  4.35, -7.26, },
+                { 5.97, -4.44,  0.27, -7.17,  6.08, },
+                { 8.23,  1.08,  9.04,  2.14, -6.87  },
+            }.ToTensor();
+
+            DenseTensor<double> b = new[,]
+            {
+                { 4.02, -1.56,  9.81,},
+                { 6.19,  4.00, -4.09,},
+                {-8.22, -8.67, -4.57,},
+                {-7.57,  1.75, -8.61,},
+                {-3.03,  2.86,  8.99 },
+            }.ToTensor();
+
+            Solve(a, b);
+
+
+            // 3*x0 + x1 = 9 
+            // x0 + 2*x1 = 8
+            a = new[,]
+            {
+                { 3.0, 1.0 },
+                { 1.0 , 2.0 },
+            }.ToTensor();
+
+            b = new[,]
+            {
+                { 9.0 },
+                { 8.0 },
+            }.ToTensor();
+
+            Solve(a, b);
+        }
+
+        static void Solve(DenseTensor<double> a, DenseTensor<double> b)
+        {
+            Console.WriteLine("Entry Matrix A");
+            Console.WriteLine(a.GetArrayString());
+            Console.WriteLine("Right Rand Side");
+            Console.WriteLine(b.GetArrayString());
+
+            var solution = LinearAlgebra.Solve(a, b);
+
+            Console.WriteLine("Solution");
+            Console.WriteLine(solution.GetArrayString());
+        }
+    }
+}
